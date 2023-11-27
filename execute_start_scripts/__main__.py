60d7797fed7cd524f0ff8460e09a3242f547a8f6
Ralf Glaser <glaser@iotmaxx.de>
2023-11-27 14:57:36 +0100

    correct imports

--- execute_start_scripts/__main__.py
+++ execute_start_scripts/__main__.py
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from .executeStartScripts import executeStartScripts
 import logging
 import time
+import argparse
 
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
